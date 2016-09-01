679f36184a98bbd2bb1a1e16b57c623f3f458f94
JonMercer <mercer.jon@gmail.com>
2016-08-31 21:44:58 -0700

    RC-1 Added player scene

---
+++ RocketCast/PlayerController.swift
@@ -0,0 +1,32 @@
+//
+//  PlayerController.swift
+//  RocketCast
+//
+//  Created by Odin on 2016-08-31.
+//  Copyright © 2016 UBCLaunchPad. All rights reserved.
+//
+
+import UIKit
+
+class PlayerController: UIViewController {
+    
+    var mainView: PlayerView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    private func setupView() {
+        let viewSize = CGRectMake(0, 0, view.bounds.width, view.bounds.height)
+        mainView = PlayerView.instancefromNib(viewSize)
+        view.addSubview(mainView!)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+}
---
+++ RocketCast/PlayerViewDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerViewDelegate.swift
+//  RocketCast
+//
+//  Created by Odin on 2016-08-31.
+//  Copyright © 2016 UBCLaunchPad. All rights reserved.
+//
+
+import UIKit
+
+protocol PlayerViewDelegate {
+    
+}
