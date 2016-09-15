e0abe30f4ce35884666c72e77f63afb05bc3678f
Milton <milleung964@gmail.com>
2016-09-14 21:42:57 -0700

    RC-23 Add assert test cases

--- RocketCast/DownloadBridge.swift
+++ RocketCast/DownloadBridge.swift
@@ -11,52 +11,56 @@ import Foundation
 import UIKit
 
 protocol DownloadBridgeProtocol {
-  func downloadPodcastXML(url:PodcastWebURL, result:(url: PodcastStorageURL) -> ())
-  func downloadImage(url: ImageWebURL, result:(url: ImageStorageURL) -> ())
-  func downloadMp3(url: MP3WebURL, result:(url: MP3StorageURL) -> ())
+    func downloadPodcastXML(url:PodcastWebURL, result:(url: PodcastStorageURL?) -> ())
+    func downloadImage(url: ImageWebURL, result:(url: ImageStorageURL) -> ())
+    func downloadMp3(url: MP3WebURL, result:(url: MP3StorageURL) -> ())
 }
 extension ModelBridge: DownloadBridgeProtocol {
-  
-  func downloadPodcastXML(url:PodcastWebURL, result:(url: PodcastStorageURL) -> ()) {
-    let podcastURL = NSURL(string: url)
     
-    let task = NSURLSession.sharedSession().dataTaskWithURL(podcastURL!) {(data, response, error) in
-      
-      guard error == nil else {
-        Log.error(error.debugDescription)
-        return
-      }
-      let XMLString = NSString(data: data!, encoding: NSUTF8StringEncoding)
-      
-      let filePathAppend = "/Documents/\(url.stringByRemovingAll(stringsToRemove)).xml"
-      
-      let filePath = NSHomeDirectory() + filePathAppend
-
-      do {
-        try XMLString!.writeToFile(filePath, atomically: true, encoding: NSUTF8StringEncoding)
-        result(url: filePathAppend)
-      } catch let error as NSError {
-        Log.error(error.debugDescription)
-      }
-      
+    func downloadPodcastXML(url:PodcastWebURL, result:(url: PodcastStorageURL?) -> ()) {
+        let podcastURL = NSURL(string: url)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(podcastURL!) {(data, response, error) in
+            
+            guard error == nil else {
+                Log.error(error.debugDescription)
+                result(url: nil)
+                return
+            }
+            guard let XMLString = NSString(data: data!, encoding: NSUTF8StringEncoding) else {
+                result(url: nil)
+                return
+            }
+            
+            let filePathAppend = "/Documents/\(url.stringByRemovingAll(stringsToRemove)).xml"
+            
+            let filePath = NSHomeDirectory() + filePathAppend
+            
+            do {
+                try XMLString.writeToFile(filePath, atomically: true, encoding: NSUTF8StringEncoding)
+                result(url: filePathAppend)
+            } catch let error as NSError {
+                Log.error(error.debugDescription)
+            }
+            
+        }
+        task.resume()
+        
+        
+        //download it
+        //pass the rss data to another object
+        // get the authrfrom that object
+        //get other info from that object (list of episodes, title of podcast)
+        //put the title, episodes, and author into the POdcastModel
+        
     }
-    task.resume()
     
+    func downloadImage(url: ImageWebURL, result:(url: ImageStorageURL) -> ()) {
+        //TODO
+    }
     
-    //download it
-    //pass the rss data to another object
-    // get the authrfrom that object
-    //get other info from that object (list of episodes, title of podcast)
-    //put the title, episodes, and author into the POdcastModel
+    func downloadMp3(url: MP3WebURL, result:(url: MP3StorageURL) -> ()) {
+        //TODO
+    }
     
-  }
-  
-  func downloadImage(url: ImageWebURL, result:(url: ImageStorageURL) -> ()) {
-    //TODO
-  }
-  
-  func downloadMp3(url: MP3WebURL, result:(url: MP3StorageURL) -> ()) {
-    //TODO
-  }
-  
 }
--- RocketCastTests/DownloadTests.swift
+++ RocketCastTests/DownloadTests.swift
@@ -10,54 +10,100 @@ import XCTest
 @testable import RocketCast
 
 class DownloadTests: XCTestCase {
-  
-  override func setUp() {
-    super.setUp()
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-  }
-  
-  override func tearDown() {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    super.tearDown()
-  }
-  
-  func testExample() {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-  }
-  
-  func testPerformanceExample() {
-    // This is an example of a performance test case.
-    self.measureBlock {
-      // Put the code you want to measure the time of here.
-    }
-  }
-  
-  func testDownloadPodcstXML() {
     
-    var done = false
-    let url = "http://billburr.libsyn.com/rss"
-    let fileMgr = NSFileManager.defaultManager()    
-    
-    ModelBridge.sharedInstance.downloadPodcastXML(url) { (downloadedPodcast) in
-      done = true
-      
-      let path = NSHomeDirectory().stringByAppendingString(downloadedPodcast)
-      
-      if let data = fileMgr.contentsAtPath(path) {
-        Log.info(String(data:data, encoding: NSUTF8StringEncoding)!)
-      }
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
-    waitUntil(6) {done}
-    XCTAssert(done, "completeion should be called")
-  }
-  
-  private func waitUntil(timeout: NSTimeInterval, predicate:(Void -> Bool)) {
-    let timeoutTime = NSDate(timeIntervalSinceNow: timeout).timeIntervalSinceReferenceDate
-    
-    while (!predicate() && NSDate.timeIntervalSinceReferenceDate() < timeoutTime) {
-      NSRunLoop.currentRunLoop().runMode(NSDefaultRunLoopMode, beforeDate: NSDate(timeIntervalSinceNow: 5))
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
-  }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func testDownloadPodcastXML() {
+        // Test real podcasts
+        var done = false
+        let url = "http://billburr.libsyn.com/rss"
+        let fileMgr = NSFileManager.defaultManager()
+        
+        ModelBridge.sharedInstance.downloadPodcastXML(url) { (downloadedPodcast) in
+            done = true
+            
+            XCTAssertNotNil(downloadedPodcast)
+            
+            let path = NSHomeDirectory().stringByAppendingString(downloadedPodcast!)
+            
+            if let data = fileMgr.contentsAtPath(path) {
+                let xmlString = String(data:data, encoding: NSUTF8StringEncoding)!
+                Log.info(xmlString)
+                XCTAssertTrue(xmlString.containsString("xml") && xmlString.containsString("xml"))
+            }
+        }
+        let url2 = "http://www.npr.org/rss/podcast.php?id=510298"
+        
+        ModelBridge.sharedInstance.downloadPodcastXML(url2) { (downloadedPodcast) in
+            done = true
+            
+            XCTAssertNotNil(downloadedPodcast)
+            
+            let path = NSHomeDirectory().stringByAppendingString(downloadedPodcast!)
+            
+            if let data = fileMgr.contentsAtPath(path) {
+                let xmlString = String(data:data, encoding: NSUTF8StringEncoding)!
+                Log.info(xmlString)
+                XCTAssertTrue(xmlString.containsString("xml") && xmlString.containsString("xml"))
+            }
+        }
+        
+        waitUntil(6) {done}
+    }
+    
+    func testDownloadRandomString() {
+        // Testing a random string
+        var done = false
+        let url = "fawefwefaw"
+        let fileMgr = NSFileManager.defaultManager()
+        
+        ModelBridge.sharedInstance.downloadPodcastXML(url) { (downloadedPodcast) in
+            done = true
+            XCTAssertNil(downloadedPodcast)
+        }
+        
+        waitUntil(6) {done}
+    }
+    
+    func testDownloadNonPodcastLinkXML() {
+        // Testing a random non podcast link
+        var done = false
+        let url = "http://www.google.ca/"
+        let fileMgr = NSFileManager.defaultManager()
+        
+        ModelBridge.sharedInstance.downloadPodcastXML(url) { (downloadedPodcast) in
+            done = true
+            XCTAssertNil(downloadedPodcast)
+        }
+        
+        waitUntil(6) {done}
+    }
+    
+    private func waitUntil(timeout: NSTimeInterval, predicate:(Void -> Bool)) {
+        let timeoutTime = NSDate(timeIntervalSinceNow: timeout).timeIntervalSinceReferenceDate
+        
+        while (!predicate() && NSDate.timeIntervalSinceReferenceDate() < timeoutTime) {
+            NSRunLoop.currentRunLoop().runMode(NSDefaultRunLoopMode, beforeDate: NSDate(timeIntervalSinceNow: 5))
+        }
+    }
 }
