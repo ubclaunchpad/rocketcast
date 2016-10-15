15237e580dfa500a6e12a156d3f6cac822ba7f27
Milton Leung <milleung964@gmail.com>
2016-10-15 15:06:04 -0700

    RC-45 add test file

---
+++ RocketCastTests/PlayerTests.swift
@@ -0,0 +1,41 @@
+//
+//  PlayerTests.swift
+//  RocketCast
+//
+//  Created by Milton Leung on 2016-10-15.
+//  Copyright © 2016 UBCLaunchPad. All rights reserved.
+//
+
+import XCTest
+import AVFoundation
+
+class PlayerTests: XCTestCase, AVAudioPlayerDelegate {
+    
+    var audioPlayer: AVAudioPlayer!
+    
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func playMP3() {
+        PlayerController
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        <#code#>
+    }
+}
